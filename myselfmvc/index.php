0c1a902c8976ed5fa589fe5b6da139d9a85d6b36
qq1007269259 <1007269259@qq.com>
2018-03-12 10:06:03 +0800

    first

---
+++ myselfmvc/index.php
@@ -0,0 +1,16 @@
+<?php
+header("content-type:text/html;charset=utf-8");
+// 应用目录为当前目录
+define('APP_PATH', __DIR__ . '/');
+
+// 开启调试模式
+define('APP_DEBUG', true);
+
+// 加载框架文件
+require(APP_PATH . 'fastphp/Fastphp.php');
+
+// 加载配置文件
+$config = require(APP_PATH . 'config/config.php');
+
+// 实例化框架类
+(new fastphp\Fastphp($config))->run();
